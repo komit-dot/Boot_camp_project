8097be67ab258077b84d42e7403aa6474c12ce19
komit-dot <komitcha@gmail.com>
2024-01-24 19:08:05 +0530

    one to one association done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_24_093120) do
+ActiveRecord::Schema.define(version: 2024_01_24_125810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,15 @@ ActiveRecord::Schema.define(version: 2024_01_24_093120) do
     t.string "alternate_contact_number"
   end
 
+  create_table "sub_demos", force: :cascade do |t|
+    t.string "title"
+    t.bigint "demo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["demo_id"], name: "index_sub_demos_on_demo_id"
+  end
+
   add_foreign_key "student_projects", "projects"
   add_foreign_key "student_projects", "students"
+  add_foreign_key "sub_demos", "demos"
 end
