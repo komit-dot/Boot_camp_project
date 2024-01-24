f14ac2d40b06aeff386bd5e26e19f70a98942781
komit-dot <komitcha@gmail.com>
2024-01-24 14:42:09 +0530

    done habtm association

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   has_many :blogs
-  before_update  :greeting_message
+  has_and_belongs_to_many :courses
   validates :first_name, :last_name, :email, presence: true
 
   def greeting_message
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_23_060819) do
+ActiveRecord::Schema.define(version: 2024_01_24_064531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 2024_01_23_060819) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.bigint "student_id", null: false
   end
 
   create_table "demos", force: :cascade do |t|
