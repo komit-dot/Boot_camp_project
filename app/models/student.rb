c5db024d34acc9d10add9d7db144b5e25c804726
komit-dot <komitcha@gmail.com>
2024-02-01 17:07:17 +0530

    works on callbacks

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,8 +5,15 @@ class Student < ApplicationRecord
   has_many :projects, through: :student_projects
   validates :first_name, :last_name, :email, presence: true
 
-  def greeting_message
-   puts("This is hello world from ruby.")
+  after_commit :display_student_age
+
+  def display_student_age
+    if self.datee_of_birth.present?
+      age = Date.today.year - self.datee_of_birth.year
+      puts("============Age of the student is:#{age}===========")
+    else
+      puts("================Age of the student is not calculated without date of birth===========.")
+    end
   end 
 
 end
