a2df80ed30e81e59acd2649bad51bbbc28eb5592
komit-dot <komitcha@gmail.com>
2024-02-06 12:58:27 +0530

    befor action and after action call back integration in students controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :print_display_message
+  after_action :after_display_message
+
+  private
+  def print_display_message
+	puts("I am an inherited beforeaction filter.")
+  end
+
+  def after_display_message
+	puts("=================I am inherited afteraction filter.")
+  end
 end
