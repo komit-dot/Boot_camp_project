fa6866f3d36fed480940c6068f61f083c63ef217
komit-dot <komitcha@gmail.com>
2024-02-07 19:21:36 +0530

    some routes practice for new routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get 'welcome/index'
   root 'welcome#index'
+
+  get 'students/test'
   resources :students
+  #get 'students/test'
   resources :blogs
+
+  get 'welcome/about'
+  get '/about' => 'welcome#about'
 end
