561bc7fb45875bd8665dcbf7287c5b917b3827b3
komit-dot <komitcha@gmail.com>
2024-02-08 11:50:20 +0530

    add scope in concern and revert changes in routes file

--- app/models/concerns/validatable.rb
+++ app/models/concerns/validatable.rb
@@ -4,6 +4,8 @@ module Validatable
   included do
   	validates :name, :description, presence: true, uniqueness: true
     validates :description, length: {minimum: 10, maximum: 1000}
+
+    scope :current_month, ->{where(created_at: Date.today.beginning_of_the_month...Date.today.created_at.end_of_month)}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,25 +6,25 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'students/test'
-  # resources :students do
-  #   member do
-  #     get :personal_details
-  #   end
+  resources :students do
+    member do
+      get :personal_details
+    end
 
-  #   #collection do
-  #     get :active
-  #   #end
-  # # use if you require only one member action use this sytex---->  get :personal_details, on: :member
-  # end
-  #get 'students/test'
+    #collection do
+      get :active
+    #end
+  # use if you require only one member action use this sytex---->  get :personal_details, on: :member
+  end
+  get 'students/test'
   resources :blogs
 
   get 'welcome/about'
   get '/about' => 'welcome#about'
 
-  namespace :admin do
-    resources :students
-  end
+  # namespace :admin do
+  #   resources :students
+  # end
 
   # scope module: :admin do
   #   resources :students
