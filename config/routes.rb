800bc480660df54b37f11fcda355390e9a38d15d
komit-dot <komitcha@gmail.com>
2024-02-14 18:39:23 +0530

    added new admin screen layout with js and jquery

--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,9 @@ Rails.application.routes.draw do
   get 'welcome/about'
   get '/about' => 'welcome#about'
 
-  # namespace :admin do
-  #   resources :students
-  # end
+  namespace :admin do
+    get 'dashboard' => 'dashboard#index'
+  end
 
   # scope module: :admin do
   #   resources :students
